b18cd091dac2863e7ee6c2c973d9777701f61817
Brandon Davis <brandon.davis@kitware.com>
2019-08-21 09:58:33 -0400

    others

--- varrock/home/.config/login.sh
+++ varrock/home/.config/login.sh
@@ -7,4 +7,5 @@ sleep 0.2
 
 sleep 0.2 # sleep before feh
 
-feh  --bg-fill /home/brandon/github.com/subdavis/dotfiles/images/firefly-upright.png --bg-fill /home/brandon/github.com/subdavis/dotfiles/images/firefly.png
+feh  --bg-fill /home/brandon/github.com/subdavis/dotfiles/images/firefly.png --bg-fill /home/brandon/github.com/subdavis/dotfiles/images/firefly-upright.png
+# feh  --bg-fill /home/brandon/github.com/subdavis/dotfiles/images/firefly-upright.png --bg-fill /home/brandon/github.com/subdavis/dotfiles/images/firefly.png
